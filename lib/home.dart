f6dc97cbd1eacf06704da9b0201211bd27e2ab3f
Cerberus <cerberusprogrammer@gmail.com>
2022-10-08 19:32:22 -0700

    Pendiente realizar un refresh en la lista de items que representan un tag para el usuario.

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,20 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
+  List<String> tags = [];
+
+  List<PopupMenuItem> makeTags() {
+    List<PopupMenuItem> textTags = [];
+
+    for (int i = 0; i < tags.length; i++) {
+      textTags.add(PopupMenuItem(
+        child: Text(tags[i]),
+      ));
+    }
+
+    return textTags;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +32,53 @@ class _Home extends State<Home> {
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add a note',
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  scrollable: true,
+                  content: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Title',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    PopupMenuButton(
+                      color: const Color.fromARGB(255, 238, 238, 238),
+                      padding: const EdgeInsets.all(10),
+                      position: PopupMenuPosition.under,
+                      tooltip: 'Tag',
+                      icon: const Icon(Icons.tag),
+                      itemBuilder: (BuildContext context) {
+                        List<PopupMenuItem<dynamic>> items = [];
+
+                        items.add(PopupMenuItem(child: TextFormField(
+                          onFieldSubmitted: (value) {
+                            tags.add(value);
+                          },
+                        )));
+                        items.addAll(makeTags());
+                        return items;
+                      },
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Add'),
+                    ),
+                  ],
+                );
+              });
+        },
       ),
     );
   }
--- lib/note.dart
+++ lib/note.dart
@@ -1,25 +1,8 @@
 class Note {
-  String _title;
-  DateTime _date;
-  String _content;
+  String title;
+  DateTime date;
+  String content;
+  String tag;
 
-  Note(this._title, this._date, this._content);
-
-  String get title => _title;
-
-  set title(String title) {
-    _title = title;
-  }
-
-  DateTime get date => _date;
-
-  set date(DateTime date) {
-    _date = date;
-  }
-
-  String get content => _content;
-
-  set content(String content) {
-    _content = content;
-  }
+  Note(this.title, this.date, this.content, this.tag);
 }
